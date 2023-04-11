135b97f6bcf6f2f2610ccda4f17dae921bcc2ccf
kuanchiliao1 <kuanchiliao@gmail.com>
2023-04-11 18:44:36 -0400

    Add logic to implement grid

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
-const containerEL = document.getElementById("container")
+const containerEl = document.getElementById("container")
 
-function renderSquares() {
-  const numberOfSquares = 16 * 16;
+function renderSquares(sideSquares) {
+  const numberOfSquares = sideSquares ** 2;
+  const squareLength = Math.floor(window.getComputedStyle(containerEl).width.slice(0, -2) / String(sideSquares))
+
+  containerEl.style.height = window.getComputedStyle(containerEl).width
+
+  const rowHeight = squareLength;
+  const rowLength = squareLength * sideSquares;
   
-  for (let i = 0; i < numberOfSquares; i++) {
-    containerEL.innerHTML += `
-      <div class="square" id=${i + 1}></div>
-    `
+  for (let i = 0; i < sideSquares; i++) {
+    containerEl.innerHTML += `
+  <div class="row" id="row-${i + 1}" style="width: ${rowLength}px; height: ${rowHeight}px;" id=${i + 1}></div>
+`
+  const rowEl = document.getElementById(`row-${i + 1}`)
+    for (let j = 0; j < sideSquares; j++) {
+      rowEl.innerHTML += `
+        <div class="square" id="${j + 1}"></div>
+      `
+    }
   }
 }
 
-renderSquares()
+containerEl.addEventListener("mouseover", (event) => {
+  console.log(event.target.id)
+  if (event.target.id) {
+    event.target.classList.add("white")
+  }
+})
+
+renderSquares(5)
