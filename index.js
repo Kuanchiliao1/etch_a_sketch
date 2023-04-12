ff2f561c4c9ed7c3acd634af5d406690967e868e
kuanchiliao1 <kuanchiliao@gmail.com>
2023-04-12 14:06:37 -0400

    Add btns

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const containerEl = document.getElementById("container")
+const resetBtnEl = document.getElementById("reset")
+const changeSizeBtnEl = document.getElementById("change-size")
+let sideSquares = 6
 
 function renderSquares(sideSquares) {
-  const numberOfSquares = sideSquares ** 2;
   const squareLength = Math.floor(window.getComputedStyle(containerEl).width.slice(0, -2) / String(sideSquares))
-
-  containerEl.style.height = window.getComputedStyle(containerEl).width
-
   const rowHeight = squareLength;
   const rowLength = squareLength * sideSquares;
+  containerEl.innerHTML = ""
   
   for (let i = 0; i < sideSquares; i++) {
     containerEl.innerHTML += `
@@ -23,10 +23,19 @@ function renderSquares(sideSquares) {
 }
 
 containerEl.addEventListener("mouseover", (event) => {
-  console.log(event.target.id)
   if (event.target.id) {
     event.target.classList.add("white")
   }
 })
 
-renderSquares(5)
+changeSizeBtnEl.addEventListener("click", (event) => {
+  sideSquares = prompt("Enter size between 1 and 30")
+  renderSquares(sideSquares)
+})
+
+resetBtnEl.addEventListener("click", (event) => {
+  containerEl.innerHTML = ""
+  renderSquares(sideSquares)
+})
+
+renderSquares(sideSquares)
